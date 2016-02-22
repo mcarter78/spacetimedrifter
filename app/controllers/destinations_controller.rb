c160ed1a032c18b360d5328397a4d9ec7ad69410
mcarter78 <sjsharksftw@gmail.com>
2016-02-22 12:43:34 -0800

    adds routes and controller methods for destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,10 @@
 class DestinationsController < ApplicationController
+  def index
+    @destinations = Destination.all
+    render :index
+  end
+  def show
+    @destination = Destination.find(:id)
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :users
-  resources :destinations
+  resources :destinations, only: [:index, :show]
 end
