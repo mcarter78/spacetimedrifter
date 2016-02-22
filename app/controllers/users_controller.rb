51da435750396994b42b644ccd2cd65693d315a5
mcarter78 <sjsharksftw@gmail.com>
2016-02-22 10:49:44 -0800

    fills in user routes and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    user = User.update_attributes(user_params)
+    redirect_to user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password_digest, :about_me, :avatar_url)
+  end
 end
